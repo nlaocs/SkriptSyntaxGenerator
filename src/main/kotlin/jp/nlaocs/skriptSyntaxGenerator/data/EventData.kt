0182487042609f163d1fbf80be7952b3be4b5232
あお <113911070+nlaocs@users.noreply.github.com>
2026-03-19 02:48:46 +0900

    refactor: Introduce EventData class extending CommonSyntaxData for event handling

---
+++ src/main/kotlin/jp/nlaocs/skriptSyntaxGenerator/data/EventData.kt
@@ -0,0 +1,29 @@
+package jp.nlaocs.skriptSyntaxGenerator.data
+
+import ch.njol.skript.registrations.EventValues
+import jp.nlaocs.skriptSyntaxGenerator.data.common.CommonSyntaxData
+import org.bukkit.event.Cancellable
+import org.bukkit.event.Event
+import org.skriptlang.skript.bukkit.registration.BukkitSyntaxInfos
+
+class EventData(s: BukkitSyntaxInfos.Event<*>) : CommonSyntaxData(s) {
+    val referenceEvents: List<Class<out Event>> = s.events().toList() // todo 本当にnullableではないのか？
+    val eventValues: List<EventValues.EventValueInfo<*, *>>? // todo nullableにすべきか
+    val cancellable: Boolean = referenceEvents
+        .all { Cancellable::class.java.isAssignableFrom(it) }
+    val hasOnPrefix: Boolean = s.name().startsWith("On ") // Nameで判断しているのは、Skriptが自動追加しているため。
+
+    init {
+        val allEventValues = EventValues.getPerEventEventValues()
+        val eventValueList = mutableListOf<EventValues.EventValueInfo<*, *>>()
+        for ((eventClass, info) in allEventValues.entries()) {
+            for (refEvent in referenceEvents) {
+                if (eventClass.isAssignableFrom(refEvent)) {
+                    eventValueList.add(info)
+                }
+            }
+
+        }
+        eventValues = eventValueList.ifEmpty { null }
+    }
+}
