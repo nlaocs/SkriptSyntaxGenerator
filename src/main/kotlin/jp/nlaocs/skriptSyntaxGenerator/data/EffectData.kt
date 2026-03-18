ce22360c8077fe2506ba7d904a893615abe86271
あお <113911070+nlaocs@users.noreply.github.com>
2026-03-19 02:34:38 +0900

    refactor: Introduce EffectData class extending CommonSyntaxData for better syntax organization

---
+++ src/main/kotlin/jp/nlaocs/skriptSyntaxGenerator/data/EffectData.kt
@@ -0,0 +1,7 @@
+package jp.nlaocs.skriptSyntaxGenerator.data
+
+import ch.njol.skript.lang.Effect
+import jp.nlaocs.skriptSyntaxGenerator.data.common.CommonSyntaxData
+import org.skriptlang.skript.registration.SyntaxInfo
+
+class EffectData(s: SyntaxInfo<out Effect>) : CommonSyntaxData(s)
