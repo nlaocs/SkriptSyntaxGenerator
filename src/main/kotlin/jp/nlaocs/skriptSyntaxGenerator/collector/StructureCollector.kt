ad3c72384b33669157cc61fece6cc17249040d78
あお <113911070+nlaocs@users.noreply.github.com>
2026-03-23 05:57:20 +0900

    refactor: Add StructureCollector class for collecting structure data from SyntaxRegistry

---
+++ src/main/kotlin/jp/nlaocs/skriptSyntaxGenerator/collector/StructureCollector.kt
@@ -0,0 +1,12 @@
+package jp.nlaocs.skriptSyntaxGenerator.collector
+
+import jp.nlaocs.skriptSyntaxGenerator.data.StructureData
+import org.skriptlang.skript.registration.SyntaxRegistry
+
+class StructureCollector(private val registry: SyntaxRegistry) : SyntaxCollector<StructureData> {
+    override val fileName = "structures.json"
+
+    override fun collect(): List<StructureData> =
+        registry.syntaxes(SyntaxRegistry.STRUCTURE)
+            .map { StructureData(it) }
+}
