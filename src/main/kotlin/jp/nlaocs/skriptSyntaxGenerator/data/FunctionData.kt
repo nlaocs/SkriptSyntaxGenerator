1757fa2ca71f1127001ff322def0d14960f37a57
あお <113911070+nlaocs@users.noreply.github.com>
2026-03-29 15:01:07 +0900

    refactor: Enhance FunctionData to include parameter details and modifiers

--- src/main/kotlin/jp/nlaocs/skriptSyntaxGenerator/data/FunctionData.kt
+++ src/main/kotlin/jp/nlaocs/skriptSyntaxGenerator/data/FunctionData.kt
@@ -2,6 +2,7 @@ package jp.nlaocs.skriptSyntaxGenerator.data
 
 import ch.njol.skript.lang.function.Function
 import jp.nlaocs.skriptSyntaxGenerator.data.common.Documentable
+import org.skriptlang.skript.common.function.Parameter
 
 class FunctionData(s: Function<*>) : Documentable {
     override val name: String? = s.name
@@ -12,6 +13,50 @@ class FunctionData(s: Function<*>) : Documentable {
     override val requires: List<String>?
     val returnType: Class<*>? = s.type()
     val returnTypeIsSingle: Boolean = s.isSingle // Expressionと違って、true/falseしか来ない
+    val parameters: List<ParameterInfo> = s.signature.parameters().all().map { ParameterInfo(it) }.toList()
+
+    data class ParameterInfo(
+        val name: String,
+        val type: Class<*>,
+        val modifiers: List<ModifierInfo>,
+        val isSingle: Boolean,
+    ) {
+        data class ModifierInfo(
+            val type: String,
+            val min: Any? = null,
+            val max: Any? = null,
+        ) {
+            companion object {
+                fun from(mod: Parameter.Modifier): ModifierInfo {
+                    return when (mod) {
+                        Parameter.Modifier.OPTIONAL ->
+                            ModifierInfo("optional")
+
+                        Parameter.Modifier.KEYED ->
+                            ModifierInfo("keyed")
+
+                        is Parameter.Modifier.RangedModifier<*> ->
+                            ModifierInfo(
+                                "range",
+                                mod.min,
+                                mod.max
+                            )
+
+                        else ->
+                            ModifierInfo("unknown")
+                    }
+                }
+            }
+        }
+
+        constructor(param: Parameter<*>) : this(
+            name = param.name(),
+            type = param.type(),
+            modifiers =
+                param.modifiers().map { ModifierInfo.from(it) }.toList(),
+            isSingle = param.isSingle,
+        )
+    }
 
     init {
         when (s) {
