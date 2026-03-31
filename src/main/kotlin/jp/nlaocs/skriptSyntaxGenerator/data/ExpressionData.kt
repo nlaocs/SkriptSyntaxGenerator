cc16f9c80987a1eb715d9b53a4df548d4cbd2ec3
あお <113911070+nlaocs@users.noreply.github.com>
2026-03-31 14:52:52 +0900

    remove: remove dynamic multiplicity handling from ExpressionData and clean up code

--- src/main/kotlin/jp/nlaocs/skriptSyntaxGenerator/data/ExpressionData.kt
+++ src/main/kotlin/jp/nlaocs/skriptSyntaxGenerator/data/ExpressionData.kt
@@ -10,16 +10,7 @@ import org.skriptlang.skript.registration.DefaultSyntaxInfos
 class ExpressionData(s: DefaultSyntaxInfos.Expression<*, *>) : CommonSyntaxData(s) {
     val returnType: Class<*>? = s.returnType() // todo nullableにすべきかを調べる
 
-    val returnTypeMultiplicity: Multiplicity? = try {
-        Multiplicity.fromBoolean(s.instance().isSingle)
-    } catch (e: Exception) {
-        Bukkit.getLogger().warning(
-            "Failed to retrieve multiplicity for expression: ${
-                s.type().annoValue<Name, String>()
-            }. Falling back to BOTH. Error: ${e.message}"
-        )
-        null
-    } // todo 動的に変動する可能性がある...?
+    // returnTypeMultiplicityで実装していたisSingleの内容は動的なため廃止
 
     val acceptedChangers: Map<Changer.ChangeMode, List<Class<*>>>? = try {
         val instance = s.instance()
@@ -34,22 +25,4 @@ class ExpressionData(s: DefaultSyntaxInfos.Expression<*, *>) : CommonSyntaxData(
         )
         null
     } // todo 直接保持しているが見直すべきかも
-
-    enum class Multiplicity {
-        SINGLE, MULTIPLE, BOTH;
-
-        fun toBoolean(): Boolean? = when (this) {
-            SINGLE -> true
-            MULTIPLE -> false
-            BOTH -> null
-        }
-
-        companion object {
-            fun fromBoolean(value: Boolean?): Multiplicity = when (value) {
-                true -> SINGLE
-                false -> MULTIPLE
-                null -> BOTH
-            }
-        }
-    }
 } // todo instanceが取得できなかった場合データの信用性が崩壊するので設計を考え直す
