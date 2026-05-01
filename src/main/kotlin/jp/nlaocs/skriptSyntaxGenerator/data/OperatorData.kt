c25b55519dddc0ea9a5e6c8b17dfc302bfca9267
あお <113911070+nlaocs@users.noreply.github.com>
2026-05-01 11:49:19 +0900

    feat: implement Addon interface in OperatorData and enhance addon information retrieval

--- src/main/kotlin/jp/nlaocs/skriptSyntaxGenerator/data/OperatorData.kt
+++ src/main/kotlin/jp/nlaocs/skriptSyntaxGenerator/data/OperatorData.kt
@@ -1,5 +1,9 @@
 package jp.nlaocs.skriptSyntaxGenerator.data
 
+import jp.nlaocs.skriptSyntaxGenerator.data.common.Addon
+import jp.nlaocs.skriptSyntaxGenerator.data.common.AddonInfo
+import jp.nlaocs.skriptSyntaxGenerator.hook.collector.RegisterOperatorCollector
+import org.bukkit.Bukkit
 import org.skriptlang.skript.lang.arithmetic.Operator as SkriptOperator
 import org.skriptlang.skript.util.Priority
 
@@ -7,7 +11,19 @@ data class OperatorData(
     val sign: String,
     val priority: Priority,
     val key: String?,
-) {
+) : Addon {
+    @Transient
+    val snapshot = RegisterOperatorCollector.getInstance()
+        .snapshotMap()[RegisterOperatorCollector.Key(sign, key)]
+
+    override val addon: AddonInfo = if (snapshot?.addonName != null && snapshot.addonVersion != null) {
+        AddonInfo(snapshot.addonName, snapshot.addonVersion)
+    } else {
+        Bukkit.getLogger()
+            .warning("Operator $sign($key) does not have addon information.")
+        AddonInfo("unknown", "unknown")
+    }
+
     constructor(operator: SkriptOperator) : this(
         sign = operator.sign(),
         priority = operator.priority(),
